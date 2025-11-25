fbd69210db5ca4eb392e276e01cd074c24edb4d5
Haytham Zaami <haythamzaami12@gmail.com>
2025-11-25 10:56:04 -0500

    added kirchoff stuff
    might switch from using prody to using my own algos for nma, maybe use library for data presentation

--- T_State_1oel.py
+++ T_State_1oel.py
@@ -6,9 +6,16 @@ atoms = prody.parsePDB('1oel')
 print(f"Total atoms: {atoms.numAtoms()}")
 print(f"Number of residues: {atoms.numResidues()}")
 
+
+
 #for nma using only c-alphas
 calphas = atoms.select('calpha')
 
+
 gnm = GNM('Chaperonin')
 gnm.buildKirchhoff(calphas, cutoff=7, gamma=1) #researchers tend to use these values wehn running GNM
-print(gnm.getKirchhoff())
+
+print("Kirchhoff")
+kirch_1oel = gnm.getKirchhoff()
+print(kirch_1oel)
+print(kirch_1oel.shape)
